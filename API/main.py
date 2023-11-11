ecc3cdb26a39ce73ddb7568bba58368f8b87a5bc
Aidan Andrews <aidanandrews112@gmail.com>
2023-11-11 16:34:20 -0700

    Barcode scanner is now a function that returns the name of the barcode's product

--- API/main.py
+++ API/main.py
@@ -14,32 +14,43 @@ import requests
 import json
 
 #openai.api_key = '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'
+
 barcodeSession = requests.Session()
 BARCODE_API_URL = "https://www.buycott.com/api/v4/products/lookup"
 BARCODE_ACCESS_TOKEN = "TB1g4mG7r2LjST04BSrLx-BtWdThbynryW-DUKm4"
 
 
+def grabItemName(barcodeID: str):
+    params = { 
+               "barcode": barcodeID,
+               "access_token": BARCODE_ACCESS_TOKEN
+              }
 
-barcodeID = "786162650351"
+    headers = {
+        "Content-Type": "application/json"
+    }
 
-params = { 
-           "barcode": barcodeID,
-           "access_token": BARCODE_ACCESS_TOKEN
-          }
-
-headers = {
-    "Content-Type": "application/json"
-}
-
-response = barcodeSession.get(BARCODE_API_URL, headers=headers, params=params)
-
-if response.status_code == 200:
-    # If the response is in JSON format, use response.json()
-    data = response.json()
-    print(data)
-else:
-    print(f"Error: {response.status_code}, {response.text}")
+    response = barcodeSession.get(BARCODE_API_URL, headers=headers, params=params)
 
 
 
-#curl -X GET -H "Content-Type: application/json" -d "{\"barcode\": \"096619281893\",\"access_token\": \"TB1g4mG7r2LjST04BSrLx-BtWdThbynryW-DUKm4\"}" "https://www.buycott.com/api/v4/products/lookup"
+    if response.status_code == 200:
+        # If the response is in JSON format, use response.json()
+        #data = response.json()
+        data = response.json()
+
+        # print(data)
+        try:
+         product_name = data["products"][0]["product_name"]
+         return product_name
+        
+        except KeyError:
+         print("ID doesn't exist")
+         return "ID doesn't exist"
+        
+    else:
+        return "Error: Invalid barcode"
+
+    
+name = grabItemName("070942001472")
+print(name)
